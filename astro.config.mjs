67f98641f6841094eac77d6f5a4d700f25e9d7f9
Imadecrypte <nail.dezzaz45@gmail.com>
2025-02-18 13:45:56 +0100

    test atro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,6 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'server',
-  adapter: node({ mode: 'standalone' }),
-  devOptions: {
-    host: true,
-    port: 4321,
-  },
+  adapter: 
+    node({ mode: 'standalone' }),
 });
